0244a2a00069941cd0676dddebbe7f8bab4bbb8a
Ilya <shilya91@gmail.com>
2024-05-23 03:18:05 +0300

    :hammer: Creating necessary directories

---
+++ prepare_filestructure.py
@@ -0,0 +1,19 @@
+import os
+
+
+def prepare_main_calc_directory() -> None:
+    if "calculations" not in os.listdir():
+        os.mkdir(
+            "calculations"
+        )
+    if "trajectory" not in os.listdir("calculations"):
+        os.mkdir(
+            "calculations/trajectory"
+        )
+
+
+def prepare_feature_calc_directory(method_name: str) -> None:
+    if method_name not in os.listdir("calculations"):
+        os.mkdir(
+            f"calculations/{method_name}"
+        )
