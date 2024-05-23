88245356611f248eb38591df72040ee91139c6e1
Ilya <shilya91@gmail.com>
2024-05-23 03:25:43 +0300

    :alembic: Calculate RMSD matrix using trajectory

---
+++ features/heavy_atoms.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+from gromacs.fileformats.xpm import XPM
+
+
+def prepare_matrix(topology_filename: str, trajectory_filename: str) -> None:
+
+    os.system(
+        f"gmx rms -f {trajectory_filename} -s {topology_filename} -m calculations/trajectory/rmsd.xpm << EOF\n"
+        "4\n"
+        "4\n"
+        "EOF\n"
+    )
+
+    rmsd_matrix = XPM(
+        "calculations/trajectory/rmsd.xpm"
+    ).array.astype(np.float32)
+
+    rmsd_matrix.tofile("calculations/heavy_atoms/rmsd.dat")
