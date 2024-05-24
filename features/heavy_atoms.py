c63d121eb0c4dba8775c6f85e9ea452498e49920
pc2 <pc2>
2024-05-24 19:35:01 +0300

    :bug: Calculate RMSD matrix using heavy atoms cartesian coordinates: bugfix

--- features/heavy_atoms.py
+++ features/heavy_atoms.py
@@ -6,7 +6,7 @@ from gromacs.fileformats.xpm import XPM
 def prepare_matrix(topology_filename: str, trajectory_filename: str) -> None:
 
     os.system(
-        f"gmx rms -f {trajectory_filename} -s {topology_filename} -m calculations/trajectory/rmsd.xpm << EOF\n"
+        f"gmx rms -f {trajectory_filename} -s {topology_filename} -o calculations/trajectory/rmsd.xvg -m calculations/trajectory/rmsd.xpm << EOF\n"
         "4\n"
         "4\n"
         "EOF\n"
