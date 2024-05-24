41be717e20fa6fbb1f8098ca7cec0af878f67ece
pc2 <pc2>
2024-05-24 19:33:04 +0300

    :bug: :building_construction: TSNE Manager: fixed "csv index" errors and made the initialization function public

--- tsne_manager.py
+++ tsne_manager.py
@@ -1,3 +1,4 @@
+from typing import Dict, Tuple
 import numpy as np
 import pandas as pd
 
@@ -6,28 +7,31 @@ class TSNEManager:
     def __init__(self, log_filename: str) -> None:
         self.log_filename = log_filename
 
-    def _init_file(self) -> None:
+    def init_file(self) -> None:
         data = pd.DataFrame(
             columns=["feature", "perplexity", "clusters", "silhouette_score"])
 
-        data.to_csv(self.log_filename)
+        data.to_csv(self.log_filename, index=False)
 
     def log(self, feature: str, perplexity: int, clusters: int, silhouette_score: float) -> None:
-        data = pd.read_csv(self.log_filename)
+        data = pd.read_csv(self.log_filename, index_col=None)
+
+        new_row = [{
+            "feature": feature,
+            "perplexity": perplexity,
+            "clusters": clusters,
+            "silhouette_score": silhouette_score
+        }]
 
         data = pd.concat([
             data,
-            pd.DataFrame({
-                "feature": feature,
-                "perplexity": perplexity,
-                "clusters": clusters,
-                "silhouette_score": silhouette_score
-            })
-        ], ignore_index=True)
+            pd.DataFrame(new_row)
+        ],
+            ignore_index=True)
 
-        data.to_csv(self.log_filename)
+        data.to_csv(self.log_filename, index=False)
 
-    def get_best(self) -> dict[str, pd.DataFrame]:
+    def get_best(self) -> Dict[str, pd.DataFrame]:
 
         data = pd.read_csv(self.log_filename)
 
@@ -42,7 +46,7 @@ class TSNEManager:
     def get_all(self) -> pd.DataFrame:
         return pd.read_csv(self.log_filename)
 
-    def get_transformed_and_labels(self, method: str, perplexity: int, clusters: int) -> tuple[np.ndarray, np.ndarray]:
+    def get_transformed_and_labels(self, method: str, perplexity: int, clusters: int) -> Tuple[np.ndarray, np.ndarray]:
         transformed = np.loadtxt(
             f"calculations/{method}/transformed/{perplexity}.dat"
         )
